9f063903c0d851b083cd913e3e59903c760b9132
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-10-09 04:37:47 -0400

    wrote a quick and dirty program to try pretty printing - have not verified yet

---
+++ prettyJSON/prettyJson.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io"
+	"bufio"
+	"os"
+	"encoding/json"
+	"log"
+)
+
+func PrettyPrint(in io.Reader, out io.Writer, indent string) (err error) {
+	var stuff interface{}
+	var writeBuf []byte
+	decoder := json.NewDecoder(in)
+	for decoder.More() {
+		err = decoder.Decode(&stuff)
+		if err != nil {
+			return err
+		}
+		writeBuf, err = json.MarshalIndent(stuff, "", indent)
+		if err != nil {
+			return err
+		}
+		out.Write(writeBuf)
+	}
+	if err == io.EOF {
+		return nil
+	} else {
+		return err
+	}
+}
+
+func main() {
+	if err := PrettyPrint(bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout), "\t"); err != nil {
+		log.Fatal(err)
+	}
+}
