ae1064a455457911f5c7094cf2aa4cf96dd5f37c
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-10-11 04:44:36 -0400

    added function to return Ints from an interface

--- functions.go
+++ functions.go
@@ -2,9 +2,23 @@ package cliTricks
 
 import (
 	"strings"
+	"fmt"
 	"strconv"
 	"errors"
 )
+
+func GetInt(data interface{}, target[]string) (int, error) {
+	var ok bool
+	var value int
+  tempItem, err := GetItem(data, target)
+  if err != nil {
+  	return -1, fmt.Errorf("bad item - %v", err)
+  }
+  if value, ok = tempItem.(int); !ok {
+  	return -1, fmt.Errorf("got non-int item - %s", tempItem)
+  }
+  return value, nil
+}
 
 func breakupStringArray(input string) []string {
 	if strings.HasPrefix(input, "[") && strings.HasSuffix(input, "]") {
@@ -26,7 +40,7 @@ func breakupStringArray(input string) []string {
 	return parts
 }
 
-func getBuriedItem(data interface{}, target []string) (interface{}, error) {
+func GetItem(data interface{}, target []string) (interface{}, error) {
 	if dataSafe, ok := data.([]interface{}); ok {
 		targetInt, err := strconv.Atoi(target[0])
 		if err != nil {
@@ -34,14 +48,14 @@ func getBuriedItem(data interface{}, target []string) (interface{}, error) {
 		}
 		if len(target) > 1{
 			// there's stuff on the inside to dive into
-			return getBuriedItem(dataSafe[targetInt], target[1:])
+			return GetItem(dataSafe[targetInt], target[1:])
 		} else {
 			return dataSafe[targetInt], nil
 		}
 	}	else if dataSafe, ok := data.(map[string]interface{}); ok {
 		if len(target) > 1{
 			// there's stuff on the inside to dive into
-			return getBuriedItem(dataSafe[target[0]], target[1:])
+			return GetItem(dataSafe[target[0]], target[1:])
 		} else {
 			return dataSafe[target[0]], nil
 		}
@@ -50,7 +64,7 @@ func getBuriedItem(data interface{}, target []string) (interface{}, error) {
 	}
 }
 
-func setBuriedItem(data, value interface{}, target []string) error {
+func SetItem(data, value interface{}, target []string) error {
 	if dataSafe, ok := data.([]interface{}); ok {
 		targetInt, err := strconv.Atoi(target[0])
 		if err != nil {
@@ -58,7 +72,7 @@ func setBuriedItem(data, value interface{}, target []string) error {
 		}
 		if len(target) > 1{
 			// there's stuff on the inside to dive into
-			return setBuriedItem(dataSafe[targetInt], value, target[1:])
+			return SetItem(dataSafe[targetInt], value, target[1:])
 		} else {
 			dataSafe[targetInt] = value
 			return nil
@@ -66,7 +80,7 @@ func setBuriedItem(data, value interface{}, target []string) error {
 	}	else if dataSafe, ok := data.(map[string]interface{}); ok {
 		if len(target) > 1{
 			// there's stuff on the inside to dive into
-			return setBuriedItem(dataSafe[target[0]], value, target[1:])
+			return SetItem(dataSafe[target[0]], value, target[1:])
 		} else {
 			dataSafe[target[0]] = value
 			return nil
